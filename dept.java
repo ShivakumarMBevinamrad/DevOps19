64b421bfa7080dc1fadaf6c722d996d2e84df553
Shivakumar <shivakumarbevinamarad62@gmail.com>
2024-03-10 21:46:11 +0400

    CI EXAM

---
+++ dept.java
@@ -0,0 +1,15 @@
+
+class dept {
+    String n="salfn";
+    public static void  department(int id)
+    {
+        if(id==101)
+        {
+            System.out.println("bca");     
+        }
+        else if(id==102)
+        {
+            System.out.println("mca");
+        }
+    }
+}
